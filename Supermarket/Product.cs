f33f23c655417fde0e26431fa753241ad56a9741
ESTEBAN <juan.cc2397@gmail.com>
2022-03-01 18:36:15 -0500

    Add project files.

---
+++ Supermarket/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace Supermarket
+{
+
+    public abstract class Product : IPay
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Tax { get; set; }
+        public abstract decimal ValueToPay();               
+        
+        public override string ToString()
+        {
+            return$"{Id} - {Description}, " +
+                $"\n\tPrice........:    {Price:N2}" +
+                $"\n\tTax..........:    {Tax*100:N2}%";
+        }
+        
+    }
+    
+}
---
+++ Supermarket/VariablePriceProduct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    public class VariablePriceProduct : Product
+    {
+        
+        public string Measurement { get; set; }
+        public decimal Quantity { get; set; }
+        public override decimal ValueToPay()
+        {
+            return Price;
+        }
+
+        public override string ToString()        
+        {
+            decimal asist = Quantity * ValueToPay();
+            decimal total = asist * Tax;
+            return $"{Id} - {Description}" +
+                $"\n\tMeasurement..:    {Measurement}" +
+                $"\n\tPrice........:    ${Price:N2}" +
+                $"\n\tQuantity.....:    {Quantity*1000:N2}gr" +
+                $"\n\tTax..........:    {Tax*100:N2}%" +
+                $"\n\tValue to Pay.:    ${asist+total:N2}";
+                
+        }
+
+
+
+    }
+}
